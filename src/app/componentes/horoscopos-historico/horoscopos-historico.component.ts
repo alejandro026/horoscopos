0aefe2c40f80e0e31e7033c4c4c37fb3a39e55dd
ARGL <argl@fsw.com.mx>
2022-11-10 16:57:13 -0600

    cambios

--- src/app/componentes/horoscopos-historico/horoscopos-historico.component.ts
+++ src/app/componentes/horoscopos-historico/horoscopos-historico.component.ts
@@ -7,9 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HoroscoposHistoricoComponent implements OnInit {
 
+  fechaHoy:Date= new Date();
+  fechaAyer?:Date;
+  fechaMa?:Date;
+
   constructor() { }
 
   ngOnInit(): void {
+    this.calcularDias();
+  }
+
+
+
+
+  calcularDias(){
+    this.fechaAyer=new Date((this.fechaHoy.getDate()-1));
+    this.fechaMa?.setDate(this.fechaMa.getDate()+1);
+
+    console.log("calcularDias ~ this.fechaMa", this.fechaMa)
+
   }
 
 }
