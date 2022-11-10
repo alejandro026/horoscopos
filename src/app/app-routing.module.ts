db26240f709c92e0a2345bf9e4531218e82bdbbe
ARGL <argl@fsw.com.mx>
2022-11-10 17:07:56 -0600

    rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -7,6 +7,7 @@ const routes: Routes = [
   { path: '', component: HoroscoposMensualComponent },
   { path: 'horoscopos', component: HoroscoposMensualComponent },
   { path: 'historico', component: HoroscoposHistoricoComponent },
+  { path: '*', component: HoroscoposHistoricoComponent },
 ];
 
 @NgModule({
