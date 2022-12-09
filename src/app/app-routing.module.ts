2efb21d8ff9aa54a041aa2e895afd00ba9d6a5c6
ARGL <argl@fsw.com.mx>
2022-12-09 12:10:08 -0600

    Cambios

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,16 +1,9 @@
-import { VirgoComponent } from './componentes/virgo/virgo.component';
-import { TauroComponent } from './componentes/tauro/tauro.component';
-import { SagitarioComponent } from './componentes/sagitario/sagitario.component';
-import { PiscisComponent } from './componentes/piscis/piscis.component';
-import { LibraComponent } from './componentes/libra/libra.component';
-import { LeoComponent } from './componentes/leo/leo.component';
 import { GeminisComponent } from './componentes/geminis/geminis.component';
 import { EscorpioComponent } from './componentes/escorpio/escorpio.component';
 import { CapricornioComponent } from './componentes/capricornio/capricornio.component';
 import { CancerComponent } from './componentes/cancer/cancer.component';
 import { AriesComponent } from './componentes/aries/aries.component';
 import { AcuarioComponent } from './componentes/acuario/acuario.component';
-import { PruebaComponent } from './componentes/prueba/prueba.component';
 import { HoroscoposMensualComponent } from './componentes/horoscopos-mensual/horoscopos-mensual.component';
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
@@ -24,13 +17,6 @@ const routes: Routes = [
   { path: 'capricornio', component:  CapricornioComponent},
   { path: 'escorpio', component:  EscorpioComponent},
   { path: 'geminis', component:  GeminisComponent},
-  { path: 'leo', component:  LeoComponent},
-  { path: 'libra', component:  LibraComponent},
-  { path: 'piscis', component:  PiscisComponent},
-  { path: 'sagitario', component:  SagitarioComponent},
-  { path: 'tauro', component:  TauroComponent},
-  { path: 'virgo', component:  VirgoComponent},
-  { path: 'prueba', component:  PruebaComponent},
 ];
 
 @NgModule({
