f7b10a9cd836d1cbedfa78dd425c237c8ab55450
ARGL <argl@fsw.com.mx>
2022-12-08 18:01:10 -0600

    Cambios

--- src/app/componentes/prueba/prueba.component.ts
+++ src/app/componentes/prueba/prueba.component.ts
@@ -8,10 +8,7 @@ import Swal from 'sweetalert2';
 })
 export class PruebaComponent implements OnInit {
 
-  likes1:number=54;
-  likes2:number=523;
-  comentarios1:number=10;
-  comentarios2:number=154;
+
 
   constructor() {
   }
@@ -19,54 +16,8 @@ export class PruebaComponent implements OnInit {
   ngOnInit(): void {
   }
 
-  subirLikes(numero:number){
-    switch (numero) {
-      case 1:
-        this.likes1++;
-        break;
-      case 2:
-        this.likes2++;
-        break;
-      default:
-        break;
-    }
-  }
 
-  dejarComentario(numero:number){
-    Swal.fire({
-      title: 'Escribe tu comentario',
-      input: 'text',
-      inputAttributes: {
-        autocapitalize: 'off'
-      },
-      showCancelButton: true,
-      confirmButtonText: 'Publicar',
-      cancelButtonText: 'Cancelar',
-      showLoaderOnConfirm: true,
-      allowOutsideClick: () => !Swal.isLoading()
-    }).then((result) => {
-      if (result.isConfirmed) {
-        Swal.fire({
-          position: 'center',
-          icon: 'success',
-          title: 'Tu comentario se ha publicado',
-          showConfirmButton: false,
-          timer: 1500
-        })
 
-        switch (numero) {
-          case 1:
-            this.comentarios1++;
-            break;
-          case 2:
-            this.comentarios2++;
-            break;
-          default:
-            break;
-        }
 
-      }
-    })
-  }
 
 }
